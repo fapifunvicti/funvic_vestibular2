07b829d9f5e94ab5d5c3d541a9f0b74e7a44a9bf
Nicholas Oliveira <nicholasluis@gmail.com>
2026-05-12 17:53:31 -0300

    script de criar banco de dados

---
+++ criar_banco.sh
@@ -0,0 +1,25 @@
+#!/bin/env bash
+
+arquivo_db="vestibular1.db"
+folder="db1"
+db_path="$folder/$arquivo_db"
+sql_dump="sql/vestibular.db.sql"
+
+if [ ! -d "$folder" ]; then
+    echo "pasta db nao existe criando.."
+    mkdir -p $folder
+    chown $USER:$USER 
+    chmod 0744 $folder
+else
+    echo "pasta db ja existe.. ignorando"
+fi
+
+if [ -f "$db_path" ]; then
+    echo "arquivo $db_path ja existe. saindo"
+    exit;
+fi
+
+sqlite3  $db_path < $sql_dump
+
+
+
