4b33a9be33d83175dafc97d7af59b307ae8e5ea5
Nicholas Oliveira <nicholasluis@gmail.com>
2026-05-13 11:13:12 -0300

    adicionado classe de tratar querystrings e filtrar dados

---
+++ app/Core/QueryString.php
@@ -0,0 +1,55 @@
+<?php
+namespace App\Core;
+
+class QueryString {
+    private array $params = [];
+
+    public function __construct(string $query_string = ''){
+
+        \parse_str($query_string, $this->params);
+        $this->sanitize();
+    }
+
+    private function sanitize(): void {
+
+        \array_walk_recursive($this->params, function(&$value){
+            $value = str_replace("\0", '', $value);
+            $value = \strip_tags($value);
+            $value = \htmlentities($value, \ENT_QUOTES | \ENT_HTML5, 'UTF-8');
+            $value = trim($value);
+
+
+        });
+
+    }
+
+    public function get(string $key, mixed $default = null): mixed {
+            return $this->params[$key] ?? $default;
+    }
+
+    public function as_int(string $key, int $default = 0): int{
+        return \filter_var($this->get($key) ?? $default, \FILTER_SANITIZE_NUMBER_INT);
+    } 
+
+    public function as_float(string $key, float $default = 0.0): int{
+        return \filter_var($this->get($key) ?? $default, \FILTER_SANITIZE_NUMBER_FLOAT | \FILTER_FLAG_ALLOW_FRACTION);
+    } 
+
+    public function as_bool(string $key, int $default = 0): float{
+        return \filter_var($this->get($key) ?? $default, \FILTER_SANITIZE_NUMBER_FLOAT | \FILTER_FLAG_ALLOW_FRACTION);
+    } 
+
+    public function as_str(string $key, string $default = ""): string {
+            return \filter_var($this->get($key) ?? $default, \FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+    }
+    
+    
+    public function has(string $key): string | null {
+        return isset($this->params[$key]) ? $this->params[$key] : null; 
+    }
+
+    public function all(): array{
+        return $this->params;
+    }
+
+}
