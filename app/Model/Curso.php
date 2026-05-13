514c7efb950e2483a08b71ac502cd86f18fa192c
Nicholas Oliveira <nicholasluis@gmail.com>
2026-05-13 14:44:32 -0300

    correcao no model cursos

--- app/Model/Curso.php
+++ app/Model/Curso.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 class Curso extends Model {
     protected $table = "curso"; 
     protected $primaryKey = 'idcurso';
-    protected $fillable = ['nome', 'ativo', 'criado_em', 'alterado_em', 'deletado_em'];
+    protected $fillable = ['idcurso', 'nome', 'ativo', 'criado_em', 'alterado_em', 'deletado_em'];
+     public $timestamps = false;
+
 }
