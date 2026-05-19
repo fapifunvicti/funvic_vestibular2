4b31e410b626cdf4868e053f371ef1a22c6d2ac7
Nicholas Oliveira <nicholasluis@gmail.com>
2026-05-19 12:00:14 -0300

    correcoa na parte de editar formulario

--- templates/default/admin/cursos/campo_nome_editavel.php
+++ templates/default/admin/cursos/campo_nome_editavel.php
@@ -7,7 +7,7 @@
 
 <?php if(isset($modo_editar) &&  $modo_editar): ?>
 
-<form hx-post="/admin/curso">
+<form hx-post="/admin/curso"  hx-target="#coluna-<?= h($curso->idcurso); ?>">
     <input type="hidden" name="form" value="editar">
     <input type="hidden" name="ativo" value="0">
     <input type="hidden" name="id" value="<?= h($curso->idcurso); ?>">
@@ -21,20 +21,24 @@
     <label class="form-check-label"  for="ativo">Ativo</label>
 </div>
 
-<div class="mb-3">
+<div class="d-flex align-items-center gap-3 mt-4">
     <button type="submit">Editar</button>
+    <button hx-swap="innerHTML"  hx-trigger="click once" hx-target="#coluna-<?= h($curso->idcurso); ?>" hx-get="/admin/curso?editar=false&id=<?= h($curso->idcurso); ?>&_=<?= time();  ?>" type="button">Cancelar</button>
 </div>
 </form>
 
 <?php else: ?>
-    <div hx-headers='{"Cache-Control": "no-cache"}'
+    <div    hx-target="#coluna-<?= h($curso->idcurso); ?>"
+            hx-headers='{"Cache-Control": "no-cache"}'
             hx-push-url="false" 
             hx-target="this" 
             hx-swap="innerHTML" 
             hx-get="/admin/curso?editar=true&id=<?= h($curso->idcurso); ?>&_=<?= time();  ?>" 
             hx-trigger="click once" 
             >
-        <?= h($curso->nome) ?>
+            <a href="javascript:void(0)">
+                <?= h($curso->nome) ?>
+            </a>
     </div>
 
 <?php endif; ?>
