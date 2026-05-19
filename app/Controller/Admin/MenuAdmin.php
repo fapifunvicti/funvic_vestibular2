cc1da314486d72ad1cc0c4c329b93734a6e1c8b3
Nicholas Oliveira <nicholasluis@gmail.com>
2026-05-19 12:01:27 -0300

    adiconado controller menu admin

---
+++ app/Controller/Admin/MenuAdmin.php
@@ -0,0 +1,53 @@
+<?php 
+namespace App\Controller\Admin;
+
+use App\Attributes\RouteAttribute;
+use App\Core\Controller;
+use App\Core\Request;
+use App\Core\Response;
+
+
+class MenuAdmin extends Controller {
+
+
+    #[RouteAttribute("/admin/menu", method:"GET")]
+    public function index(Request $request, Response $response): Response {
+        global $config;
+        $tpl = new \App\Core\Template($request, $config);
+        \App\Core\DB::get();
+
+        $query = $request->query_string;
+
+        if($query->has('editar') && $query->is_int('id')){
+            $id =    $query->as_int('id');
+           // $editar = $query->as_bool('editar');
+
+            $menu = new \App\Model\MenuItem()->where('idmenu','=', $id)->first();
+
+            $menuList = new \App\Model\ArvoreMenuView()->cursor();
+
+            $html = $tpl->renderTemplateFile("admin/menu/form_editar.php", ['menu' => $menu, 'lista_menu' => $menuList ]);
+
+
+            return $response->html($html);
+        }
+
+        
+
+
+        $menu = new \App\Model\ArvoreMenuView();
+
+    
+
+        $tpl->addTemplate("admin/tpl/header.php", ['titulo' => "MENU"])
+            ->addTemplate("admin/partes/topo.php", ['titulo' => "MENU DO SITE"])
+            ->addTemplate("admin/partes/menu.php")
+            ->addTemplate("admin/menu/index.php", ['menu' => $menu])
+            ->addTemplate("admin/tpl/footer.php");
+        return $response->html($tpl->renderTemplate());
+    }
+
+
+}
+
+
