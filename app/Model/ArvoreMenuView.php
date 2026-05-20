bd848e2ec3d872ceaecd5fc9459c221583f26281
Nicholas Oliveira <nicholasluis@gmail.com>
2026-05-20 16:43:51 -0300

    correcao nos tipos de dados Scope

--- app/Model/ArvoreMenuView.php
+++ app/Model/ArvoreMenuView.php
@@ -1,7 +1,9 @@
 <?php
 namespace App\Model;
 
+use Illuminate\Database\Eloquent\Attributes\Scope;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
 
 class ArvoreMenuView extends Model
 {
@@ -10,13 +12,17 @@ class ArvoreMenuView extends Model
     public $primaryKey = 'idmenu';
     public $readonly = true;  // view não aceita escrita
 
-    public function scopeRaiz($query)
+
+
+    
+    public function scopeRaiz(Builder $query): Builder
     {
         return $query->where('nivel', 0);
     }
 
-    public function scopeFilhosDe($query, int $paiId)
+    public function scopeFilhosDe(Builder $query, int $paiId): Builder
     {
         return $query->where('pai_id', '=', $paiId);
     }
+    
 }
