7d3127d384780529fd399056bf7c4b198d7609b7
Nicholas Oliveira <nicholasluis@gmail.com>
2026-05-21 11:52:07 -0300

    adicionado menu

--- templates/default/admin/partes/menu.php
+++ templates/default/admin/partes/menu.php
@@ -7,5 +7,6 @@
     <ul class="sidebar-menu">      
         <li><a href="/admin/curso"><i class="fas fa-home"></i> <span class="menu-text">CURSOS</span></a></li>
         <li><a href="/admin/menu"><i class="fas fa-home"></i> <span class="menu-text">MENU VESTIBULAR</span></a></li>
+        <li><a href="/admin/processo"><i class="fas fa-home"></i> <span class="menu-text">PROCESSO SELETIVO</span></a></li>
     </ul>
 </div>
