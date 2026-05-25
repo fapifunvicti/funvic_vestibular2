292ac9b84d7837c7883533041bc6e86a2096a6e7
Nicholas Oliveira <nicholasluis@gmail.com>
2026-05-25 14:57:13 -0300

    formulariod e resultados adicionado com checagem

--- templates/default/home/resultado.php
+++ templates/default/home/resultado.php
@@ -1,23 +1,44 @@
-<div class="main-content" id="mainContent">
-    <h2 class="g-3 mt-3">Escolha o Resultado:</h2>
+<?php
+    /***
+     * @var mixed $processos
+     */
+?>
+<main class="mb-3" role="main">
+    <div class="container">
+        <h2 class="mb-3">Resultados</h2>
 
-    <div class="form-row">
-          <?php if($processos->count() > 0): ?>
-        <form action="resultado" method="post">
-            <div class="form-group col-lg-12">
-                <label for="processo">Processo Seletivo:</label>
-              
-                <select class="form-control" name="processo" id="processo">
-                    <?php foreach($processos->cursor() as $processo): ?>
-                    <option value=""></option>
-                    <?php endforeach; ?>
-                </select>
-               
-            </div>
-        </form>
-         <?php else: ?>
-                    <p>Desculpe Nenhum Resultado Disponivel no Momento!</p>
-                <?php endif; ?>
+            <?php if($processos->count() > 0): ?>
+            <form action="/resultado" method="post">
+                <div class="mb-3 mt-3">
+                    <label class="form-label" for="processo">Processo Seletivo:</label>
+                
+                    <select class="form-control" name="processo" id="processo">
+                        <?php foreach($processos->cursor() as $processo): ?>
+                        <?php
+                            if($processo->data_prova):
+                                $data_prova = DateTime::createFromFormat('Y-m-d H:i:s', $processo->data_prova);
+                        ?>
+                            <option value="<?= h($processo->idprocesso);  ?>"><?= h($processo->nome ?? "Processo Seletivo"); ?> - <?= h($data_prova->format("d/m/Y"));  ?></option>
+                        <?php else: ?>
+                            <option value="<?= h($processo->idprocesso);  ?>"><?= h($processo->nome ?? "Processo Seletivo"); ?></option>
+                        <?php  endif; ?>
+                        <?php endforeach; ?>
+                    </select>
+                </div>
+
+                <div class="mb-3">
+                    <label class="form-label" for="cpf">CPF:</label>
+                    <input class="form-control" required type="text"  value="" pattern="[\p{L}\p{N}\- ]+" id="cpf" name="cpf">
+                </div>
+
+
+                <div class="mb-3">
+                    <button class="btn btn-primary" type="submit">Ir para Resultado</button>
+                </div>
+
+            </form>
+            <?php else: ?>
+                        <p>Desculpe Nenhum Resultado Disponivel no Momento!</p>
+                    <?php endif; ?>
     </div>
-
-</div>
+</main>
