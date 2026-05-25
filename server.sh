bbd739e6da32102a6ce3f88b9176082e2d356fd1
Nicholas Oliveira <nicholasluis@gmail.com>
2026-05-25 11:16:22 -0300

    rodar na rede local

--- server.sh
+++ server.sh
@@ -6,4 +6,4 @@ export XDEBUG_CONFIG="client_host=127.0.0.1 client_port=9003"
 export PHPRC="$(pwd)"
 
 
-php -c ./custom.ini -S localhost:8000 -t public route_resource.php
+php -c ./custom.ini -S 10.0.3.65:8000 -t public route_resource.php
