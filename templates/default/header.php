dc587239a5578ccb40ac6641ce76759828897ae1
Nicholas Oliveira <nicholasluis@gmail.com>
2026-05-25 11:16:09 -0300

    correcao de caminho de js falhando

--- templates/default/header.php
+++ templates/default/header.php
@@ -13,10 +13,10 @@
     <link rel="stylesheet" href="<?= get_url("assets/css/banner.css"); ?>">
     <link rel="stylesheet" href="<?= get_url("assets/css/processo_seletivo.css"); ?>">
 
-    <script src="assets//js/jquery-3.7.1.js"></script>
+    <script src="/assets/js/jquery-3.7.1.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-    <script src="assets/js/jquery.mask.min.js"></script>
+    <script src="/assets/js/jquery.mask.min.js"></script>
 
 </head>
 <body>
