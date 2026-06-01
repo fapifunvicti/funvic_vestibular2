4eeb9b4e3fa02410c787ba0c41f2f687bd5ef524
Nicholas Oliveira <nicholasluis@gmail.com>
2026-06-01 10:33:40 -0300

    adicoinado model processo seletivo

---
+++ app/Model/ProcessoSeletivo.php
@@ -0,0 +1,16 @@
+<?php
+namespace App\Model;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ProcessoSeletivo extends Model {
+
+    protected $table = "processo_seletivo";
+    protected $primaryKey = 'idprocesso';
+    protected $fillable = [ 'nome', 'fk_curso', 'fk_coligada', 'fk_ensino',
+                            'data_prova', 'id_totvs', 'habilitar_resultado', 'data_resultado_inicio',
+                            'data_resultado_fim', 'inserido_em', 'deletado_em', 'alterado_em',
+                            'categoria', 'tipo_resultado'
+    ];
+
+}
