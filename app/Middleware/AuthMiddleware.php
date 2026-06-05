4f8d5597170cd4c7806d4f27ef3d52fe8afd6724
Nicholas Oliveira <nicholasluis@gmail.com>
2026-06-05 10:41:26 -0300

    auth de login que checa se a pagina e admin e faz o direcionamento

--- app/Middleware/AuthMiddleware.php
+++ app/Middleware/AuthMiddleware.php
@@ -1,15 +1,28 @@
 <?php
 namespace App\Middleware;
 
+use App\Core\Response;
 use App\Interfaces\MiddlewareInterface;
 use App\Core\Request;
 
 class AuthMiddleware implements MiddlewareInterface {
     public function handle(Request $request, callable $next): mixed{
-        
+        $response = new Response();
 
-        $req = $request;
+        $uri = $request->getUri();
+
+        if($uri == "/admin/login"){
+            if($request->getSession("user") !== null){
+                $response->redirect("/admin/processos")->send();
+                return $next($request);
+            }
+            
+            return $next($request);
+        }
     
+
+
+        $response->redirect("/admin/login")->send();
         return $next($request);
     }
 }
