9b254f4bfaea358793e18cdf045b1577eaf5dc7c
Nicholas Oliveira <nicholasluis@gmail.com>
2026-06-05 09:48:31 -0300

    adicionado pipeline pois closure é impossivel de debugar no PHP simplesmnete cospe o resultado e nao posso controlar o fluxo. com a classe pipleine faz o mesmo servico porem tenho controle

---
+++ app/Core/Pipeline.php
@@ -0,0 +1,36 @@
+<?php
+namespace App\Core;
+class Pipeline {
+    private array $middlewares;
+    private \App\Core\Controller $controller;
+    private string $method;
+    private array $params;
+
+    public function __construct(array $middlewares, Controller $controller, string $method, array $params)
+    {   
+        $this->middlewares = $middlewares;
+        $this->controller = $controller;
+        $this->method = $method;
+        $this->params = $params;
+       
+    }
+
+    public function Run(Request $request): mixed {
+        return $this->runMiddleware($request,0);
+    } 
+
+    private function runMiddleware(Request $request, int $index): mixed {
+        if($index >= count($this->middlewares)){
+            return \call_user_func_array([$this->controller, $this->method], $this->params);
+        }
+
+        $middlewareClass = $this->middlewares[$index];
+        $middlewareInstance = new $middlewareClass();
+
+        return $middlewareInstance->handle($request, function(Request $request) use ($index){
+            return $this->runMiddleware($request, $index+1);
+        });
+
+    }
+
+}
