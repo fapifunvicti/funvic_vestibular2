e711d35b76249f544c75c32530f1e7b501ef887a
Nicholas Oliveira <nicholasluis@gmail.com>
2026-06-10 14:08:36 -0300

    modal de documentos igual do site original

---
+++ templates/default/partes/documentos_modal.inc.php
@@ -0,0 +1,40 @@
+
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Documentos Necessários para a Matrícula</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+
+            <div class="modal-body">
+                <div class="container">
+                        <p>OBS: Todos os documentos deverão ser apresentados em duas vias (cópias) com o original para conferência. Todas em folhas de tamanho A4. Poderá ser solicitado o documento complementar posteriormente caso necessário.</p>
+                        <li>Certidão de conclusão do curso de Ensino Médio ou Equivalente</li>
+                        <li>Histórico Escolar do curso de Ensino Médio ou Equivalente</li>                                                                                                                                                                                   
+                        <li>Título de Eleitor ou Protocolo</li>
+                        <li>Comprovante de votação</li>
+                        <li>Certidão de Nascimento ou Casamento;</li>
+                        <li>Cadastro de Pessoa Física (CPF) ou protocolo de solicitação;</li>
+                        <li>Duas fotos 3x4;</li>
+                        <li>Comprovante de Endereço atualizado;</li>
+                        <li>Certificado de Reservista ou Atestado de Alistamento Militar ou Atestado de Matrícula em CPOR ou NPOR, p/ brasileiros maiores de 18 anos, do sexo masculino.</li>
+
+                        <br>
+
+                        <p>Documentos do garantidor solidário: 2 cópias de cada.</p>
+
+                        <br>
+
+                        <li>Cédula de Identidade (RG);</li>
+                        <li>Cadastro de Pessoa Física (CPF);</li>
+                        <li>Comprovante de endereço atualizado.</li>
+                    </ul>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
