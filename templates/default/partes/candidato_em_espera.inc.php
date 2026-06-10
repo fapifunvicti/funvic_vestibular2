c471a6237d8a04f3725cfa302ffe0e09f1e3ac48
Nicholas Oliveira <nicholasluis@gmail.com>
2026-06-10 14:07:24 -0300

    adiconado template de resultado dos candidatos

---
+++ templates/default/partes/candidato_em_espera.inc.php
@@ -0,0 +1,24 @@
+<form class="" method="post" accept-charset="utf-8" enctype="application/x-www-form-urlencoded">
+
+
+    <main role="main" class="container" style="margin-top: 10%;">
+        <div class="container">
+            <div class="text-center mt-5 pt-5">
+                <h1 style="font-size: 100px;"><b>:)</b></h1><br>
+                <h1><b>Informação Importante:</b></h1><br>
+                <p class="lead" style="color: #7c7c7c; font-size: 18px;"><i>Informamos que você está na lista de espera do Vestibular 2026. Aguarde.</i></p>
+
+                <br>
+
+                <h5><a href="https://api.whatsapp.com/send?phone=5512996507709" target="_blank">Clique aqui para mais informações.</a></h5>
+                <small>Não conseguiu acessar o link? Nosso WhatsApp é (12)99650-7709.</small>
+
+                <br><br>
+
+                <a class="btn btn-voltar" href="./resultado.php">Voltar</a>
+
+                <br><br>
+
+            </div>
+        </div>
+    </main>
---
+++ templates/default/partes/candidato_erro.inc.php
@@ -0,0 +1,25 @@
+<form class="" method="post" accept-charset="utf-8" enctype="application/x-www-form-urlencoded">
+
+
+    <main role="main" class="container" style="margin-top: 10%;">
+        <div class="container">
+            <div class="text-center mt-5 pt-5">
+                <h1 style="font-size: 100px;"><b>:(</b></h1><br>
+                <h1><b>Ops...</b></h1><br>
+                <p class="lead" style="color: #7c7c7c; font-size: 18px;"><i>CPF não consta na classificação, ou não realizou o "PROCESSO" ou não obteve pontuação mínima necessária.</i></p>
+
+                <br>
+
+                <h5><a href="https://api.whatsapp.com/send?phone=5512996507709" target="_blank">Clique aqui para mais informações.</a></h5>
+                <small>Não conseguiu acessar o link? Nosso WhatsApp é (12)99650-7709.</small>
+
+                <br><br>
+
+                <a class="btn btn-voltar" href="/resultado">Voltar</a>
+
+                <br><br>
+
+            </div>
+        </div>
+    </main>
+
