f5df82b64995658b6fbcf97efc94428f31dbf8a6
Nicholas Oliveira <nicholasluis@gmail.com>
2026-06-12 18:02:57 -0300

    adicionado camada function total_processos_cologadas

--- app/Model/ProcessoView.php
+++ app/Model/ProcessoView.php
@@ -14,6 +14,11 @@ class ProcessoView extends Model
 
 
 
+
+    public function scopeTotalColigadas(Builder $query,  int $coligada_id){
+        return $query->selectRaw("SELECT total_processos_coligadas(?) as total", [$coligada_id]);
+    }
+
     
     public function scopeListarPorColigada(Builder $query, int $id): Builder
     {
