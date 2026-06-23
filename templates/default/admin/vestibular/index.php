5ed5529dd0814ce41511e488704e4f8675fb8f7e
Nicholas Oliveira <nicholasluis@gmail.com>
2026-06-23 15:56:51 -0300

    adicionado index paar vestibular

--- templates/default/admin/vestibular/index.php
+++ templates/default/admin/vestibular/index.php
@@ -1,3 +1,8 @@
+<?php
+    /***
+     * @var mixed $vestibular
+     */
+?>
 
 <div class="main-content" id="mainContent">
     
@@ -6,13 +11,26 @@
             <tr>
                 <th>NOME</th>
                 <th>Processos:</th>
+                <th>Ativo</th>
             </tr>
         </thead>
         <tbody>
+            <?php foreach($vestibular as $v): ?>
             <tr>
-                <td>A</td>
-                <td>B</td>
+                <td><?=  h($v->nome); ?></td>
+                <td>
+                    <a href="/admin/vestibular/<?= h($v->idvestibular);  ?>">Ver Processos</a>
+                </td>
+                <td>
+                    <div class="form-check mb-3">
+                        <div class="form-check mb-3">
+                            <label for="ativo-<?= h($v->idvestibular); ?>">Ativo</label>
+                            <input <?= $v->deletado_em === null ? 'checked' : ''  ?>  ativo-dinamico="true" data-id="<?= h($v->idvestibular)  ?>" class="form-check-input" type="checkbox" value="<?= $v->deletado_em === null ? '0' : '1' ?>" name="ativo" id="ativo-<?= h($v->idvestibular); ?>">
+                        </div>
+                    </div>
+                </td>
             </tr>
+            <?php endforeach; ?>
         </tbody>
         <tfoot>
             <tr>
@@ -22,3 +40,18 @@
         </tfoot>
     </table>
 </div>
+
+<script>
+    $(function(){
+            $('input[ativo-dinamico="true"]').on('change', function(){
+                const id = $(this).data('id');
+                $.post('/admin/vestibular',{
+                'ativo': this.checked ? 1 : 0,
+                'id': id
+                }).done(function(d){
+
+                });
+            });
+
+    });
+</script>
