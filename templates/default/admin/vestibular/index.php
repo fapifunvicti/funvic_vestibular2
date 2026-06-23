796d81d2059a5610491a4972e526f2efc789f8e2
Nicholas Oliveira <nicholasluis@gmail.com>
2026-06-23 11:58:08 -0300

    adiconado pagina inicial painel admin

---
+++ templates/default/admin/vestibular/index.php
@@ -0,0 +1,24 @@
+
+<div class="main-content" id="mainContent">
+    
+    <table class="table table-striped">
+        <thead>
+            <tr>
+                <th>NOME</th>
+                <th>Processos:</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>A</td>
+                <td>B</td>
+            </tr>
+        </tbody>
+        <tfoot>
+            <tr>
+                <th>NOME</th>
+                <th>Processos:</th>
+            </tr>
+        </tfoot>
+    </table>
+</div>
