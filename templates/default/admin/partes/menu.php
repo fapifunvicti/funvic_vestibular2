79573cccdafaad4cac604d8b2927da5bd85b497d
Nicholas Oliveira <nicholasluis@gmail.com>
2026-06-23 11:57:32 -0300

    adiconado menu nova opcao

--- templates/default/admin/partes/menu.php
+++ templates/default/admin/partes/menu.php
@@ -8,5 +8,6 @@
         <li><a href="/admin/curso"><i class="fas fa-home"></i> <span class="menu-text">CURSOS</span></a></li>
         <li><a href="/admin/menu"><i class="fas fa-home"></i> <span class="menu-text">MENU VESTIBULAR</span></a></li>
         <li><a href="/admin/processo"><i class="fas fa-home"></i> <span class="menu-text">PROCESSO SELETIVO</span></a></li>
+        <li><a href="/admin/vestibular"><i class="fas fa-home"></i> <span class="menu-text">VESTIBULAR</span></a></li>
     </ul>
 </div>
