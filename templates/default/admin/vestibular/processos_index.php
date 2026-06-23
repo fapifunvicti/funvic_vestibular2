01bd74748046620a746a6cae88c878d05d910029
Nicholas Oliveira <nicholasluis@gmail.com>
2026-06-23 15:57:06 -0300

    adicionado pagina de processos

---
+++ templates/default/admin/vestibular/processos_index.php
@@ -0,0 +1,41 @@
+<?php
+    /***
+     * @var mixed $processo
+     */
+
+
+?>
+
+<div class="main-content" id="mainContent">
+    
+    <table class="table table-striped">
+        <thead>
+            <tr>
+                <th>Coligada</th>
+                <th>Processo</th>
+                <th>Data da Prova:</th>
+                <th>Açoes</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach($processo->cursor() as $p): ?>
+            <tr>
+                <td><?= h($p->coligada_nome);  ?></td>
+                <td><?= h($p->nome);  ?></td>
+                <td><?= h($p->vestibular_data_prova_fmt)   ?></td>
+                <td>
+                    <div class="mb-3">
+                        <a  href="" class="btn btn-sm btn-danger"><i class="fa fa-trash" aria-hidden="true"></i></a>
+                    </div>
+                </td>
+            </tr>
+            <?php endforeach; ?>
+        </tbody>
+        <tfoot>
+            <tr>
+                <th>NOME</th>
+                <th>Processos:</th>
+            </tr>
+        </tfoot>
+    </table>
+</div>
