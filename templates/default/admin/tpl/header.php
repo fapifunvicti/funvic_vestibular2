e41cfb53e3d74ba854cdfadcbab9f3f6dc0dbe06
Nicholas Oliveira <nicholasluis@gmail.com>
2026-06-30 10:25:32 -0300

    adiconado font awesome para icones

--- templates/default/admin/tpl/header.php
+++ templates/default/admin/tpl/header.php
@@ -31,5 +31,7 @@
     <script src="assets/js/jquery.mask.min.js"></script>
     <script src="<?= get_url("assets/js/htmx.js"); ?>"></script>
 
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css" integrity="sha512-2SwdPD6INVrV/lHTZbO2nodKhrnDdJK9/kg2XD1r9uGqPo1cUbujc+IYdlYdEErWNu69gVcYgdxlmVmzTWnetw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
 </head>
 <body>
