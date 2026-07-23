7b9fa65643216bd1f58fa0b6f5be1930e8c49c4b
Nicholas Oliveira <nicholasluis@gmail.com>
2026-07-23 13:25:23 -0300

    correcaono layout index.php agora os processos ficam alinhados corretamente com o uso de banco de dados

--- templates/default/home/index.php
+++ templates/default/home/index.php
@@ -1,71 +1,77 @@
 <?php 
 use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Support\Facades\DB;
 
-
-
-$capsule = \App\Core\DB::get();
-
-
-    /**
-     * @var Builder $processos;
-     * @var Builder $coligadas;
-     * @var Builder $vestibulares;
-     */
+/**
+ * @var Builder $processos;
+ * @var Builder $coligadas;
+ * @var Builder $vestibulares;
+ */
 
 
 $colunas = 0;
 const MAX_COLUNAS  = 2;
 
+
 ?>
-
-
 <div class="inscricoes-info">
-    
-    <?php if($vestibulares->count() > 0): ?>
-  
-    <?php foreach($vestibulares as $vest): ?>
+    <?php if($vestibulares->count() <= 0): ?>
+            <p class="text-center text-small texto-negrito4 texto-maiusculo">
+                Desculpe, nenhum vestibular ativo no momento.
+            </p>
+    <?php else: ?>
+
+        <?php foreach($vestibulares->whereNull('deletado_em')->cursor() as $vest): ?>
         <div class="titulos">
             <h1 class="inscricao_title">Inscrições </h1>
             <h5 class="inscricao_subtitle text-secondary"><?= h($vest->nome); ?></h5>
         </div>
+        
+        <div class="w-100 cursos-grid-container  mt-4">
+             <?php foreach($coligadas->cursor() as $c): ?>
+                <div class="cursos-grid-item">
+                    <p class="text-center text-small texto-negrito4 texto-maiusculo" style="margin: 0;">
+                        <?= h($c->nome);  ?>                       
+                    </p>
+                    
+        
+                    <div class="button-container">
+                    <?php foreach($processos->whereNull('deletado_em')->where('fk_coligada', '=', $c->idcoligada)->get() as $p): ?>
 
-         <div class="w-100 cursos-grid-container  mt-4">
-            <?php foreach($processos->where('vestibular_id','=', $vest->idvestibular) ->cursor() as $p): ?>
-            <div class="cursos-grid-item">
-                <p class="text-center text-small texto-negrito4 texto-maiusculo" style="margin: 0;">
-                        <?= h($p->coligada_nome);  ?>                       
-                </p>
-                <div class="button-container">
-                    <?php
-                        $id = $p->id_totvs ?? "0";
-                        $categoria = $p->categoria ?? "1";
-                        $link_inscricao = "https://fundacaouniversitaria151485.rm.cloudtotvs.com.br/FrameHTML/web/app/Edu/PortalProcessoSeletivo/?c=1&f=1&ct={$categoria}&ps={$id}#/es/inscricoeswizard/dados-basicos";
+                            <?php
+                                $id = $p->id_totvs ?? "1";
+                                $categoria = $p->categoria ?? "1";
+                                $link_inscricao = "https://fundacaouniversitaria151485.rm.cloudtotvs.com.br/FrameHTML/web/app/Edu/PortalProcessoSeletivo/?c=1&f=1&ct={$categoria}&ps={$id}#/es/inscricoeswizard/dados-basicos";
+                            ?>
+                            <a target="_blank" href="<?=  $link_inscricao; ?>">
+                                <button type="button" class="btn botao">        
+                                    <strong class="text-uppercase"><?= h($p->nome ?? "Processo Seletivo sem Nome");  ?></strong>
+                                </button>
+                            </a>
+                      
+
+                    <?php 
+                        $colunas++;
+                        if($colunas >= MAX_COLUNAS) $colunas = MAX_COLUNAS;
+                        else $colunas = 1;
                     ?>
-                    <a target="_blank" href="<?=  $link_inscricao; ?>">
-                        <button type="button" class="btn botao">        
-                            <strong class="text-uppercase"><?= h($p->ensino_nome ?? "Processo Seletivo sem Nome");  ?></strong>
-                        </button>
-                    </a>
+                    <?php endforeach; ?>
+                    </div>
                 </div>
-            </div>
-            
-            <?php 
-                $colunas++;
-                if($colunas >= MAX_COLUNAS) $colunas = MAX_COLUNAS;
-                else $colunas = 1;
-            ?>
+
+
             <?php endforeach; ?>
-         </div>
 
-    <?php endforeach; ?>
+            
 
-    <?php else: ?>
-            <p class="text-center text-small texto-negrito4 texto-maiusculo">
-                Desculpe, nenhum vestibular ativo no momento.
-            </p>
+
+        </div>
+
+        <?php endforeach; ?>
+
     <?php endif; ?>
 </div>
+
+
 
 <style>
  .cursos-grid-container {
